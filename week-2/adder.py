fece4c264d1c512cc346e39a1790b3d0d5ef1f37
Mike Nystoriak <mxn1803@rit.edu>
2022-01-23 12:56:56 -0500

    Finished GUI and added docstrings.

--- week-2/adder.py
+++ week-2/adder.py
@@ -1,9 +1,34 @@
+"""Adds two numbers provided by the user.
+
+A command line utility that prompts the user for two numbers and prints their
+sum. Core functionality may also be hooked into by an external module.
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Mike Nystoriak'
+__credits__ = ['Mike Nystoriak']
+
 class Adder:
+    """Performs the addition of two numbers."""
+
     def add(self, a=0, b=0):
+        """Hook into the core functionality of an Adder.
+
+        Args:
+            a (int): The first number, default is 0.
+            b (int): The second number, default is 0.
+
+        Returns:
+            int|str: The sum of the two numbers. If an error occured, this will
+            be a string containing the error message.
+        """
+
         a, b, err = self.__sanitize(a, b)
         return a + b if not err else err
 
     def prompt(self):
+        """Runs a small command line interface."""
+
         a = raw_input('Please enter a number: ')
         b = raw_input('Please enter another number: ')
         a, b, err = self.__sanitize(a, b)
@@ -18,8 +43,11 @@ class Adder:
         try:
             return int(a), int(b), None
         except ValueError:
-            return None, None, '*** Error: Both entries must be integers! ***'
-
+            return (
+                None,
+                None,
+                '*** Error: Both entries must be integers! ***'
+            )
 
 def main():
     adder = Adder()
--- week-2/main.py
+++ week-2/main.py
@@ -1,8 +1,30 @@
+"""Adds two numbers provided by the user.
+
+A PyQt4 application that accepts two numbers and calculates their sum.
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Mike Nystoriak'
+__credits__ = ['Mike Nystoriak']
+
 import sys
-from PyQt4 import QtCore, QtGui
+
+from PyQt4.QtCore import Qt
+from PyQt4.QtGui import (
+    QApplication,
+    QWidget,
+    QGridLayout,
+    QLineEdit,
+    QSizePolicy,
+    QPushButton,
+    QLabel
+)
+
 from adder import Adder
 
-class AdderWidget(QtGui.QWidget):
+class AdderWidget(QWidget):
+    """Wraps a PyQt4 application around the Adder class."""
+
     def __init__(self):
         super(AdderWidget, self).__init__()
 
@@ -31,7 +53,7 @@ class AdderWidget(QtGui.QWidget):
         
         btn = self.__q_push_button('Get Sum')
 
-        layout = QtGui.QGridLayout()
+        layout = QGridLayout()
         layout.addWidget(a_input_label, 0, 0)
         layout.addWidget(self.a_input, 0, 1)
         layout.addWidget(b_input_label, 1, 0)
@@ -51,37 +73,37 @@ class AdderWidget(QtGui.QWidget):
         self.move(640, 480)
 
     def __q_line_edit(self, text, editable=True):
-        qle = QtGui.QLineEdit(text)
+        qle = QLineEdit(text)
         qle.setTextMargins(8, 8, 8, 8)
-        qle.setAlignment(QtCore.Qt.AlignRight)
+        qle.setAlignment(Qt.AlignRight)
         qle.setReadOnly(not editable)
         qle.setSizePolicy(
-            QtGui.QSizePolicy.Expanding,
-            QtGui.QSizePolicy.Expanding
+            QSizePolicy.Expanding,
+            QSizePolicy.Expanding
         )
         return qle
 
     def __q_push_button(self, text):
-        qpb = QtGui.QPushButton(text)
+        qpb = QPushButton(text)
         qpb.setSizePolicy(
-            QtGui.QSizePolicy.Expanding,
-            QtGui.QSizePolicy.Expanding
+            QSizePolicy.Expanding,
+            QSizePolicy.Expanding
         )
         qpb.setStyleSheet('background-color: #8fd485;')
         return qpb
 
     def __q_label(self, text):
-        ql = QtGui.QLabel(text)
-        ql.setAlignment(QtCore.Qt.AlignCenter)
+        ql = QLabel(text)
+        ql.setAlignment(Qt.AlignCenter)
         ql.setSizePolicy(
-            QtGui.QSizePolicy.Expanding,
-            QtGui.QSizePolicy.Expanding
+            QSizePolicy.Expanding,
+            QSizePolicy.Expanding
         )
         return ql
 
 
 def main():
-    app = QtGui.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     adderWidget = AdderWidget()
     sys.exit(app.exec_())
 
