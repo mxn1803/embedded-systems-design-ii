ed6aa56220635a205fc1f6d820a2f5d02e87afdd
Mike Nystoriak <mxn1803@rit.edu>
2022-02-14 15:25:23 -0500

    Minor touchup.

--- lab-3/ball_extractor/extractor.py
+++ lab-3/ball_extractor/extractor.py
@@ -14,17 +14,19 @@ import cv2
 import numpy as np
 
 from path import PathAccumulator
+from config import ConfigParser
 
 class BallExtractor:
     """Identifies and extracts a white ping-pong ball from an image."""
 
     def __init__(self):
         self.__path_accumulator = PathAccumulator(['.jpg', '.jpeg'])
+        self.__config_parser = ConfigParser()
 
     def prompt(self):
-        config, err = self.__parse_args(sys.argv[1:])
+        config, err = self.__config_parser.parse(sys.argv[1:])
         if err:
-            print('\n{}\n\n{}\n').format(err, self.__usage())
+            print('\n{}\n\n{}\n').format(err, self.__config_parser.usage())
             return
 
         paths = self.__build_paths(config)
@@ -63,6 +65,7 @@ class BallExtractor:
 
         paths = self.__path_accumulator.path_walk(paths)
         raws = load_raws(paths)
+        results, result_masks = [], []
 
         for raw in raws:
             masked, mask = mask_raw(raw)
@@ -78,97 +81,29 @@ class BallExtractor:
             )
             draw_circles(circles, raw)
             draw_circles(circles, mask)
-            cv2.imshow('Results', raw)
+            results.append(raw)
+            result_masks.append(mask)
+
+            cv2.imshow('Image', raw)
             cv2.imshow('Mask', mask)
             cv2.waitKey(0)
+
+        # self.__save_results(config, raw, mask)
+
         cv2.destroyAllWindows()
         return circles
+
+    def __save_results(self, config, results, masks):
+        if not os.path.isdir(config['output']):
+            os.makedirs(os.path.join(config['output'], 'results'))
+            os.makedirs(os.path.join(config['output'], 'masks'))
+        for i in range(len(results)):
+            cv2.imwrite(config['output'])
 
     def __build_paths(self, config):
         files = config['files']
         directories = config['directories']
         return self.__path_accumulator.path_walk(files + directories)
-
-    def __parse_args(self, args):
-        # if no arguments provided, print usage
-        if len(args) == 0:
-            print('\n{}\n'.format(self.__usage()))
-            exit(0)
-
-        # see if `-h` or `--help` was invoked first
-        for arg in args:
-            if arg == '-h' or arg == '--help':
-                print('\n{}\n'.format(self.__usage()))
-                exit(0)
-
-        # extract configuration
-        flags = []
-        values = []
-        for i in range(len(args)):
-            if i % 2 == 0:
-                flags.append(args[i])
-            else:
-                values.append(args[i])
-
-        # should have one key per one value
-        if len(flags) != len(values):
-            return (
-                None,
-                '*** Error: Invalid number of arguments! ***'
-            )
-
-        arg_dict = dict.fromkeys(flags)
-        for i in range(len(flags)):
-            key = flags[i]
-            value = values[i]
-            if not arg_dict[key]:
-                arg_dict[key] = []
-            arg_dict[key].append(value)
-
-        # override defaults
-        defaults = {'files': [],'directories': []}
-        config = defaults.copy()
-        d_overriden = False
-        for flag, values in arg_dict.iteritems():
-            if flag == '-f' or flag == '--file':
-                config['files'] = config['files'] + values
-            elif flag == '-d' or flag == '--directory':
-                if not d_overriden:
-                    config['directories'] = values
-                    d_overriden = True
-                else:
-                    config['directories'] = config['directories'] + values
-            else:
-                return (
-                    None,
-                    '*** Error: Invalid argument `{}`! ***'.format(flag)
-                )
-        return config, None
-
-    def __usage(self):
-        return ('Usage: python ball_extractor.py [options]'
-                '\n'
-                '\n    options: -f, --file      FILE         The path to a'
-                '\n                                          single image file'
-                '\n                                          (JPG format only).'
-                '\n                                          Many single image'
-                '\n                                          flags can be'
-                '\n                                          passed in'
-                '\n                                          succession for'
-                '\n                                          processing.'
-                '\n'
-                '\n             -d, --directory DIRECTORY    Directory with'
-                '\n                                          image files (JPG'
-                '\n                                          format only). This'
-                '\n                                          flag gives the'
-                '\n                                          user the ability'
-                '\n                                          to funnel many'
-                '\n                                          images into the'
-                '\n                                          processor at once'
-                '\n                                          with one flag.'
-                '\n'
-                '\n             -h, --help                   Show this help'
-                '\n                                          message and exit.')
 
 def main():
     ball_extractor = BallExtractor()
