6e47cfe667aedac8e6c1617d7f5c911562b934d0
Matt Evers <mge3599@rit.edu>
2022-03-04 10:53:46 -0500

    Added frequency from Websocket

--- lab-4/app/webapp/script.js
+++ lab-4/app/webapp/script.js
@@ -1,12 +1,11 @@
 let count = 5;
 update(count);
 
+// listen for keyboard input
 window.addEventListener('keydown', function(event) {
     change_freq(event.key);
     event.preventDefault();
 }, true);
-
-
 
 // create websocket connection
 const socket = new WebSocket('ws://localhost:3000')
@@ -19,57 +18,44 @@ socket.addEventListener('open', evt => {
 // listen for incoming messages
 socket.addEventListener('message', evt => {
     console.log(`Server: ${evt.data}`)
-
-    /*******************************/
-
-
-      // ADD STUFF HERE...
-
-      // if 0, dim screen
-      // else bright screen
-
-
-    /*******************************/
-
+    count = Math.round(evt.data)
+    update(evt.data)
 })
 
 
 // Change Frequency w keys
-function change_freq(name){
-  if (name == 'a') {
+function change_freq(key){
+  if (key == 'a') {
     if (count == 0) {count = 0;}
     else {count--;}
-    update(count);
-    socket.send(count)
   }
-  else if (name =='d'){
+  else if (key =='d'){
     if (count == 10){count = 10;}
     else {count++;}
-    update(count);
-    socket.send(count)
   }
+  update(count);
+  socket.send(count);
 }
 
-// Update display
+// Update frequency heading display
 function update(frequency){
   document.getElementById("freq").innerHTML = frequency + " Hz";
   color(frequency);
 }
 
 
-//Update Page (LED) color
+// Update Page (LED) color
 function color(frequency){
   background=document.querySelector("main")
   if (frequency == 0){
     background.style.backgroundColor = 'black';
     document.getElementById("freq").style.color = 'white';
+
+
+    //Is there a way to actually dim screen or did you just mean turn it black?
   }
   else {
-    background.style.backgroundColor = "green";
+    background.style.backgroundColor = "#60F360";
     document.getElementById("freq").style.color = 'black';
   }
 }
-
-//set up web socket connection
-//read/write freq
-// change to freq to be fpga readable
